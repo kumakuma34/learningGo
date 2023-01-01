030371b328454850a17278b116438ec4c49af73a
메시징개발팀 장현수 <hyun.soo@sk.com>
2023-01-01 21:48:53 +0900

    feat: loop문

--- main.go
+++ main.go
@@ -16,10 +16,31 @@ func multiply(a, b int) int {
 func repeatMe(words ...string) {
 	fmt.Println(words)
 }
-func main() {
-	totalLength, upperName := lenAndUpper("hyunsoo")
-	fmt.Println(totalLength, upperName)
-	fmt.Println(multiply(2, 2))
-	repeatMe("sfd", "dsfd", "dsf", "sdf", "sfd")
 
+// naked return
+func nakedReturn(name string) (length int, uppercase string) {
+	defer fmt.Println("I'm doen") //함수가 끝났을 때 무엇을 할지 선언을 해 줄 수 있는 함수
+	length = 1                    //위에 선언문에서 이미 생성되어 있기 때문에 선언하면 안되고 값을 대입해야 함
+	uppercase = strings.ToUpper(name)
+	return
+}
+
+func superAdd(numbers ...int) int {
+	for index, number := range numbers {
+		fmt.Println(index, number) //range는 인덱스와 값을 리턴함
+	}
+
+	for i := 0; i < len(numbers); i++ {
+		fmt.Println(numbers[i])
+	}
+
+	return 1
+}
+
+func main() {
+	superAdd(1, 2, 3, 4, 5, 6)
+	//totalLength, upperName := lenAndUpper("hyunsoo")
+	//fmt.Println(totalLength, upperName)
+	//fmt.Println(multiply(2, 2))
+	//repeatMe("sfd", "dsfd", "dsf", "sdf", "sfd")
 }
